dd21040443912ed8c61e1cb2a6281132001910c1
m. pabst <johndoe@example.com>
2023-12-30 17:39:21 +0100

    add movie

--- psychophysics/src/visual/stimuli/mod.rs
+++ psychophysics/src/visual/stimuli/mod.rs
@@ -1,8 +1,10 @@
 pub mod base;
 pub mod gratings;
 pub mod image;
+#[cfg(target_arch = "wasm32")]
 pub mod video;
 
 pub use gratings::GratingsStimulus;
 pub use image::ImageStimulus;
+#[cfg(target_arch = "wasm32")]
 pub use video::VideoStimulus;
