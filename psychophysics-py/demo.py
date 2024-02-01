c625b79bf9998db95d6b34b65fb46c18d5c97e50
m. pabst <johndoe@example.com>
2024-02-01 12:42:16 +0000

    remove unused crates

--- psychophysics-py/demo.py
+++ psychophysics-py/demo.py
@@ -4,13 +4,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import threading
 import psychophysics_py as psy
 
 # Create an experiment manager
 em = psy.ExperimentManager()
-# Get a monitor (0 is usually the laptop screen, 1 the first external
+# Get a monitor (0 is usually the internal screen, 1 the first external
 # monitor, etc.)
 monitor = em.get_available_monitors()[-1]
+
+some_global_variable = 0
 
 # Use the full screen of the second monitor at 60 Hz, select the highest
 # resolution available
@@ -20,6 +23,7 @@ win_opts = psy.WindowOptions(mode = "fullscreen_highest_resolution",
 
 # Define the experiment
 def my_experiment(window):
+     # print the thread id
     colors = [(1, 0, 0), (0, 0, 1)]
     color_index = 0
     stim = psy.ShapeStimulus(window, (0, 0, 0))
@@ -29,7 +33,7 @@ def my_experiment(window):
         frame.add(stim)
         window.submit_frame(frame)
 
-        # every 4 frames, switch the color
+       
 
         color_index = (color_index + 1) % len(colors)
         stim.set_color(colors[color_index])
