c4812c6f0158d71549c0b22e1df65d9c72512286
m. pabst <johndoe@example.com>
2024-02-22 22:12:24 +0000

    change backend

--- psychophysics/src/lib.rs
+++ psychophysics/src/lib.rs
@@ -381,7 +381,7 @@ impl ExperimentManager {
                     label: None,
                     required_features: wgpu::Features::empty(),
                     // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-                    required_limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    required_limits: wgpu::Limits::default()
                         .using_resolution(adapter.limits()),
                 },
                 None,
