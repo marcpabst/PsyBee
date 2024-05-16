a45d7ecc800b7288b99f42a8251c0a60ef55a141
m. pabst <johndoe@example.com>
2024-05-16 15:01:48 +0100

    fix instal script for manylinux

--- scripts/install_gstreamer_manylinux.sh
+++ scripts/install_gstreamer_manylinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools
+apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
