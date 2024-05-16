bacfdcb9ea542fae10bb26d5e114d5b6e26e1989
m. pabst <johndoe@example.com>
2024-05-16 16:52:19 +0100

    remove targets

--- scripts/install_gstreamer_manylinux.sh
+++ scripts/install_gstreamer_manylinux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# remove pkgconf
+yum remove -y pkgconf
+# install the real pkg-config
+yum install -y pkgconfig
 yum install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-plugins-base-devel
 
 # print the version of gstreamer
@@ -6,3 +10,9 @@ gst-inspect-1.0 --version
 
 # see if streamer-1.0.pc is in the pkg-config path
 pkg-config --cflags --libs gstreamer-1.0
+
+# run ldconfig
+ldconfig
+
+# list /usr/local/lib/pkgconfig
+ls /usr/local/lib/pkgconfig
