86fe0790cca42610757da0238d86375a94f2b185
m. pabst <johndoe@example.com>
2024-05-16 14:51:57 +0100

    fix instal script for manylinux

--- scripts/install_gstreamer_manylinux.sh
+++ scripts/install_gstreamer_manylinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/dnf install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-doc gstreamer1-plugins-base-devel gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-devel gstreamer1-plugins-bad-free-extras
+yum install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-doc gstreamer1-plugins-base-devel gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-devel gstreamer1-plugins-bad-free-extras
