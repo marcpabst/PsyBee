2adde78d74ba88a5015bdda48a1387a1f8a27e54
m. pabst <johndoe@example.com>
2024-05-16 15:13:57 +0100

    remove targets

--- scripts/install_gstreamer_manylinux.sh
+++ scripts/install_gstreamer_manylinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yum install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-plugins-base-devel gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-devel gstreamer1-plugins-bad-free-extras
+yum install -y alsa-lib-devel gstreamer1-devel gstreamer1-plugins-base-tools
 pkg-config --cflags --libs gstreamer-1.0
