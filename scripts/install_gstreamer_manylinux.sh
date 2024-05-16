3b6225dd748a1ca87570b08a9e5e37d9a261e76c
m. pabst <johndoe@example.com>
2024-05-16 17:54:05 +0100

    update PKG_CONFIG_PATH

--- scripts/install_gstreamer_manylinux.sh
+++ scripts/install_gstreamer_manylinux.sh
@@ -2,6 +2,12 @@
 
 dnf install -y python3-pip alsa-lib-devel gstreamer1 gstreamer1-devel
 
+# print python version
+python3 --version
+
+# print pip version
+pip3 --version
+
 # list /usr/local/lib/pkgconfig
 echo "Contents of /usr/local/lib/pkgconfig"
 ls /usr/local/lib/pkgconfig
